d5ab489cbc5b6198b9360350d46db40997ba17c7
nahcnats <nahcnats@gmail.com>
2022-09-14 15:05:55 +0800

    netlify forms

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export default function ContactForm() {
     const {
@@ -11,10 +12,47 @@ export default function ContactForm() {
         return <div className="r mt-2 text-xs text-red-500">{message}</div>;
     }
 
-    async function submitHandler() {}
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(
+                (key) =>
+                    encodeURIComponent(key) +
+                    "=" +
+                    encodeURIComponent(data[key])
+            )
+            .join("&");
+    };
+
+    async function submitContactHandler({ name, email, message }) {
+        const payload = {
+            name,
+            email,
+            message,
+        };
+
+        try {
+            const response = await axios({
+                method: "post",
+                url: "/",
+                data: encode({ "form-name": "contact v1", payload }),
+            });
+
+            if (response) {
+                alert("Success!");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
-        <form className="w-full" onSubmit={handleSubmit(submitHandler)}>
+        <form
+            className="w-full"
+            name="contact v1"
+            method="post"
+            data-netlify="true"
+            onSubmit={handleSubmit(submitContactHandler)}
+        >
             <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
                 <div className="mb-4 flex flex-col">
                     <label htmlFor="name">Name</label>
@@ -24,10 +62,12 @@ export default function ContactForm() {
                         })}
                         type="text"
                         id="name"
-                        className="h-8 rounded p-2"
+                        className="h-8 rounded p-2 text-black"
                     />
                     {errors.name && (
-                        <FormFieldErrors message={errors.name.message} />
+                        <FormFieldErrors
+                            message={errors.prospectName.message}
+                        />
                     )}
                 </div>
                 <div className="mb-4 flex flex-col">
@@ -42,7 +82,7 @@ export default function ContactForm() {
                         })}
                         type="email"
                         id="email"
-                        className="h-8 rounded p-2"
+                        className="h-8 rounded p-2 text-black"
                     />
                     {errors.email && (
                         <FormFieldErrors message={errors.email.message} />
@@ -57,7 +97,7 @@ export default function ContactForm() {
                         id="message"
                         cols="30"
                         rows="5"
-                        className="rounded p-2"
+                        className="rounded p-2 text-black"
                     />
                     {errors.message && (
                         <FormFieldErrors message={errors.message.message} />
